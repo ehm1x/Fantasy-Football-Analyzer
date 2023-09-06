27fc19f39b9d762ce57c68817847e8345649b796
ehm1x <125445610+ehm1x@users.noreply.github.com>
2023-09-06 15:26:35 -0400

    Add files via upload

---
+++ classes.cpp
@@ -0,0 +1,63 @@
+#include <vector>
+#include <string>  
+class Team; 
+
+
+class Player {
+public:
+
+	std::string name; 
+	float adp; 
+	std::vector<int> projPoints = {};
+	int projTotal = 0;
+	std::string position = "undef";
+
+	Player(std::string pname, const float adp1, const int projTotal1, std::string position1) {
+		name = pname;
+		adp = adp1;
+		projTotal = projTotal1;
+	}
+	Player(std::string pname, const float adp1) {
+		name = pname;
+		adp = adp1; 
+	}
+	Player() {
+		name = "Unidentified ";
+		adp = 0;
+		projTotal = 0;
+		position = "Undef";
+	}
+};
+
+
+class Team {
+public:
+	std::string teamName;
+	const float avgADP = 0;
+	std::vector<Player> roster = {};
+	std::string owner_id;
+
+	Team(std::string tName, std::string owner) {
+		teamName = tName;
+		owner_id = owner;
+	}
+	Team() {}; 
+
+	void initPlayer(Player player) {
+		roster.push_back(player);
+	}
+
+	float find_adp() {
+		int size = roster.size() - 1;
+		float total = 0.0;
+		for (auto& player : roster) {
+			player.adp += total;
+		}
+		if (size) total / -size; 
+		else {
+			return 0.0f; 
+		}
+	
+		return total;
+	}
+};
